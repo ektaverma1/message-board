442a856b56d2ed1cc0e4f9f44454be1ba6ce8bd0
Ekta Verma <ekta_verma@external.mckinsey.com>
2017-06-14 16:16:44 +0530

    Updating Sqlite3 syntax that doesnt work in postgres in Heroku

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ApplicationRecord
 
     scope :search_posts, ->(params) {joins("left join comments on comments.post_id = posts.id").where("posts.user_id = ? or comments.user_id= ? ",params[:user_id],params[:user_id]) }
 
-    scope :contains_blog_keyword, ->(params) { where("title LIKE ? or body LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")}
+    scope :contains_blog_keyword, ->(params) { where("title ILIKE ? or body ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")}
 
   	def is_it_published?
       unless self.published_at.nil?
